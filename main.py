d4d2d557e6a15f33ba7feb26d8a63cf017182a31
Swayam Sai Kar <kalyanikar527@gmail.com>
2022-03-08 10:29:18 +0530

    Created A Flask api for our article recommendation system

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from crypt import methods
 import csv
 from flask import Flask, jsonify, request
-from tqdm import main
+# importing files
 
+from demographicFiltering import output
+from contentBasedFiltering import get_recommendations
 
 all_articles = []
 
@@ -25,14 +26,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def getArticles():
-    return jsonify({"data": all_articles[0], "status": 200})
+    articles_data = {
+        # url == 11
+        # title == 12
+        # text == 13
+        # language == 14
+        "url": all_articles[0][11],
+        "title": all_articles[0][12],
+        "text": all_articles[0][13],
+        "lang": all_articles[0][14],
+    }
+    return jsonify({"data": articles_data, "status": 200})
 
 
 @app.route("/liked_articles", methods=["POST"])
 def liked_articles():
     article = all_articles[0]
-    all_articles = all_articles[1]
     liked_articles.append(article)
+    all_articles.pop(0)
 
     return jsonify({"liked_articles_data": liked_articles, "status": 200})
 
@@ -40,11 +51,39 @@ def liked_articles():
 @app.route('/non_liked_articles', methods=["POST"])
 def non_liked_articles():
     article = all_articles[0]
-    all_articles = all_articles[1]
     non_liked_articles.append(article)
+    all_articles.pop(0)
 
     return jsonify({"non_liked_articles": non_liked_articles, "status": 200})
 
 
+@app.route("/recommended_articles")
+def recommended_articles():
+    all_recommended_articles = []
+    for each_liked_article in liked_articles:
+        output = get_recommendations(each_liked_article[4])
+        for data in output:
+            all_recommended_articles.append(data)
+    import itertools
+    all_recommended_articles.sort()
+    all_recommended_articles = list(all_recommended_articles for all_recommended_articles,
+                                    _ in itertools.groupby(all_recommended_articles))
+    article_data = []
+    for each_article in all_recommended_articles:
+        obj_data = {
+            "url": each_article[0],
+            "title": each_article[1],
+            "text": each_article[2],
+            "lang": each_article[3],
+        }
+        article_data.append(obj_data)
+    return jsonify({
+        "data": article_data,
+        "status": "success"
+    }), 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+# gg 
